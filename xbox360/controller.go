b8cc8495084b13188035e6e4b499ad97997e4301
Philipp Erhardt <philipperhardt29@gmail.com>
2019-07-07 15:14:26 +0200

    Controller: Try again after 2 seconds if it couldn't be found

--- xbox360/controller.go
+++ xbox360/controller.go
@@ -1,6 +1,6 @@
 package xbox360
+
 import (
-	"io"
 	"log"
 	"math"
 	"time"
@@ -9,40 +9,47 @@ import (
 	"github.com/xarantolus/gover/rover"
 )
 
-const (
-	serialName = "Microsoft Corp. Xbox 360 Wireless Adapter"
-)
-
-var (
-	controllerPort io.ReadWriteCloser
-)
-
 func init() {
-	js, err := joystick.Open(0)
-	if err != nil {
-		log.Println("Error while setting up controller:", err.Error())
-	}
-
-	tick := time.NewTicker(time.Millisecond * 100)
-
-	var rov = rover.Current()
-
 	go func() {
-		defer js.Close()
-		defer tick.Stop()
-
-		defer rov.Stop() // In case of disconnect
-
 		for {
-			state, err := js.Read()
+			// TODO: Check if this stops instantly after booting if the controller isn't connected yet
+			js, err := joystick.Open(0)
 			if err != nil {
-				log.Printf("Controller disconnected (error: %s); you need to restart in order to reconnect", err.Error())
-				break
+				log.Println("Error while setting up controller:", err.Error())
+				time.Sleep(time.Second * 2)
+				continue
 			}
-			lr, fr := leftStickInterpret(state.AxisData[0]), buttonsInterpret(state.Buttons)
 
-			rov.SetDirection(interpretDirections(lr, fr))
-			<-tick.C
+			tick := time.NewTicker(time.Millisecond * 100)
+
+			var rov = rover.Current()
+			stopped := make(chan bool)
+			go func() {
+				defer js.Close()
+				defer tick.Stop()
+
+				defer rov.Stop() // In case of disconnect
+
+				for {
+					state, err := js.Read()
+					if err != nil {
+						log.Printf("Controller disconnected (error: %s); you need to restart in order to reconnect", err.Error())
+						break
+					}
+
+					_, _, rest := buttonsCheckXY(state.Buttons)
+
+					lr, fr := leftStickInterpret(state.AxisData[0]), buttonsFRInterpret(rest)
+
+					rov.SetDirection(interpretDirections(lr, fr))
+					<-tick.C
+				}
+
+				stopped <- true
+			}()
+
+			<-stopped
+			time.Sleep(2500 * time.Millisecond)
 		}
 	}()
 }
@@ -59,13 +66,13 @@ func interpretDirections(lr, fr rover.Direction) rover.Direction {
 	}
 	if fr == rover.Forward && finalDir == rover.Stop {
 		// Overwrite Stop with forward
-		finalDir = fr
+		finalDir = fr // TODO: Maybe only go left or right if A is also pressed (fr == Forward)
 	}
 
 	if fr == rover.Reverse {
 		switch lr {
 		case rover.Left:
-			finalDir = rover.ReverseLeft // TODO: Might have to switch these
+			finalDir = rover.ReverseLeft
 		case rover.Right:
 			finalDir = rover.ReverseRight
 		case rover.Stop:
@@ -91,7 +98,8 @@ func leftStickInterpret(axisStateZero int) rover.Direction {
 }
 
 // A = 2; B = 1; X = 4; Y = 8
-func buttonsInterpret(buttonCombinedNumber uint32) rover.Direction {
+// buttonsFRInterpret checks whether to go forward, reverse or stop
+func buttonsFRInterpret(buttonCombinedNumber uint32) rover.Direction {
 	if buttonCombinedNumber == 2 {
 		return rover.Forward
 	}
@@ -103,3 +111,24 @@ func buttonsInterpret(buttonCombinedNumber uint32) rover.Direction {
 	return rover.Stop
 }
 
+// buttonsCheckXY checks if any of the X or Y buttons are pressed
+func buttonsCheckXY(buttonCombinedNumber uint32) (haveX, haveY bool, rest uint32) {
+	rest = buttonCombinedNumber
+
+	if rest > 1000 {
+		// Is using the left control cross or the RB/LB button
+		return
+	}
+
+	if rest >= 8 {
+		haveY = true
+		rest -= 8
+	}
+
+	if rest >= 4 {
+		haveX = true
+		rest -= 4
+	}
+
+	return
+}
